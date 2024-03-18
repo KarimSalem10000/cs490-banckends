06700a315e5f208c7909fdd3fcc19b8c81c278f7
KarimSalem9000 <KarimSalem9000@gmail.com>
2024-03-18 02:36:53 -0400

    everything

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy.orm import joinedload
 import datetime
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
@@ -28,6 +29,7 @@ class Actor(db.Model):
 
 class Category(db.Model):
     __tablename__ = 'category'
+    film_id = db.Column(db.Integer, db.ForeignKey('film.film_id'))
     category_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25))
     last_update = db.Column(db.DateTime, default=datetime.datetime.now)
@@ -46,6 +48,47 @@ class City(db.Model):
     def __init__(self,city,country_id):
         self.city = city
         self.country_id = country_id
+class Language(db.Model):
+    __tablename__ = 'language'
+    language_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    last_update = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self,name):
+        self.name = name
+
+class FilmActor(db.Model):
+    __tablename__ = 'film_actor'
+    actor_id = db.Column(db.Integer, db.ForeignKey('actor.actor_id'))
+    film_id = db.Column(db.Integer, db.ForeignKey('film.film_id'), primary_key=True)
+    last_update = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self,actor_id,film_id):
+        self.actor_id = actor_id
+        self.film_id = film_id
+
+class Rental(db.Model):
+    __tablename__ = 'rental'
+    rental_id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'))
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.inventory_id'))
+    rental_date = db.Column(db.DateTime, default=datetime.datetime.now)
+    return_date = db.Column(db.DateTime)
+    staff_id = db.Column(db.Integer)
+    last_update = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self,customer_id,rental_date,return_date,staff_id,inventory_id):
+        self.customer_id = customer_id
+        self.rental_date = rental_date
+        self.return_date = return_date
+        self.staff_id = staff_id
+        self.inventory_id = inventory_id
+    
+class FilmCategory(db.Model):
+    __tablename__ = 'film_category'
+    film_id = db.Column(db.Integer, db.ForeignKey('film.film_id'), primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.category_id'))
+    last_update = db.Column(db.DateTime, default=datetime.datetime.now)
 
 class Film(db.Model):
     __tablename__ = 'film'
@@ -54,6 +97,7 @@ class Film(db.Model):
     description = db.Column(db.Text)
     release_year = db.Column(db.String(4))
     language_id = db.Column(db.Integer)
+    original_language_id = db.Column(db.Integer)
     rental_duration = db.Column(db.Integer)
     rental_rate = db.Column(db.Float)
     length = db.Column(db.Integer)
@@ -103,8 +147,16 @@ class Address(db.Model):
         self.postal_code = postal_code
         self.phone = phone
 
+class Inventory(db.Model):
+    __tablename__ = 'inventory'
+    inventory_id = db.Column(db.Integer, primary_key=True)
+    film_id = db.Column(db.Integer, db.ForeignKey('film.film_id'))
+    store_id = db.Column(db.Integer)
+    last_update = db.Column(db.DateTime, default=datetime.datetime.now)
 
-
+    def __init__(self, film_id, store_id):
+        self.film_id = film_id
+        self.store_id = store_id
 
 class Customer(db.Model):
     __tablename__ = 'customer'
@@ -114,13 +166,16 @@ class Customer(db.Model):
     last_name = db.Column(db.String(45))
     email = db.Column(db.String(50))
     address_id = db.Column(db.Integer)
-    active = db.Column(db.Boolean)
+    active = db.Column(db.Boolean, default=True)
     create_date = db.Column(db.DateTime, default=datetime.datetime.now)
     last_update = db.Column(db.DateTime, default=datetime.datetime.now)
 
-    def __init__(self,first_name,last_name):
+    def __init__(self, first_name, last_name, email, address_id, store_id):
         self.first_name = first_name
         self.last_name = last_name
+        self.email = email
+        self.address_id = address_id
+        self.store_id = store_id
 
 class CustomerSchema(ma.Schema):
     class Meta:
@@ -189,6 +244,7 @@ def movieinfo(film_id):
             'description': film.description,
             'release_year': film.release_year,
             'language_id': film.language_id,
+            'original_language_id': film.original_language_id,
             'rental_duration': film.rental_duration,
             'rental_rate': film.rental_rate,
             'length': film.length,
@@ -319,7 +375,7 @@ def listusers():
 
 @app.route('/listuser/<customer_id>', methods=['GET']) 
 def list_user(customer_id):
-    # Execute the SQL query to retrieve user details along with their currently rented DVDs
+    # Execute the SQL query to retrieve user details along with their currently r ented DVDs
     query_result = db.session.execute(text('''
         SELECT c.customer_id, c.first_name, c.last_name, c.email, 
                a.address, a.postal_code, a.phone, COUNT(r.rental_id) AS rental_count,
@@ -407,7 +463,125 @@ def user(customer_id):
     else:
         # Return a 404 response if the customer with the provided ID is not found
         return jsonify({'message': 'Customer not found'}), 404
+    
+@app.route('/listfilms', methods=['GET'])
+def listfilms():
+    films = Film.query.all()
+    film_list = []
+    for film in films:
+        film_data = {
+            'title': film.title,
+        }
+        film_list.append(film_data)
+    
+    # Return film details as JSON
+    return jsonify(film_list)
 
+@app.route('/listfilmes', methods=['GET'])
+def list_filmes():
+    films_with_category = db.session.query(Film.film_id, Film.title, Category.category_id, Category.name)\
+        .join(FilmCategory, Film.film_id == FilmCategory.film_id)\
+        .join(Category, FilmCategory.category_id == Category.category_id)\
+        .all()
+    films_with_actors = db.session.query(Film.title, Actor.first_name, Actor.last_name)\
+        .join(FilmActor, Film.film_id == FilmActor.film_id)\
+        .join(Actor, FilmActor.actor_id == Actor.actor_id)\
+        .all()
+
+    result = []
+    for film_id, title, category_id, category_name in films_with_category:
+        film_data = {
+            'film_id': film_id,
+            'title': title,
+            'category_id': category_id,
+            'category_name': category_name
+        }
+        result.append(film_data)
+
+    return jsonify(result)
+
+@app.route('/listfilmsy', methods=['GET'])
+def list_films_with_actors():
+    films_with_actors = db.session.query(Film.title, Actor.first_name, Actor.last_name)\
+        .join(FilmActor, Film.film_id == FilmActor.film_id)\
+        .join(Actor, FilmActor.actor_id == Actor.actor_id)\
+        .all()
+
+    result = []
+    for title, first_name, last_name in films_with_actors:
+        film_data = {
+            'title': title,
+            'actors': [{'first_name': first_name, 'last_name': last_name}]
+        }
+        result.append(film_data)
+
+    return jsonify(result)
+
+@app.route('/listfilmos', methods=['GET'])
+def list_films_with_category_and_actors():
+    films_with_details = db.session.query(Film.film_id, Film.title, Film.description, Film.release_year,
+                                          Film.length, Film.rating, Category.category_id, Category.name,
+                                          Actor.first_name, Actor.last_name)\
+        .join(FilmCategory, Film.film_id == FilmCategory.film_id)\
+        .join(Category, FilmCategory.category_id == Category.category_id)\
+        .join(FilmActor, Film.film_id == FilmActor.film_id)\
+        .join(Actor, FilmActor.actor_id == Actor.actor_id)\
+        .order_by(Film.film_id)\
+        .all()
+
+    result = []
+    for film_id, title, description, release_year, length, rating, category_id, category_name, first_name, last_name in films_with_details:
+        # Check if the film is already in the result list
+        existing_film = next((film for film in result if film['film_id'] == film_id), None)
+        if existing_film:
+            # If the film is already in the result list, append the actor to its 'actors' list
+            existing_film['actors'].append({'first_name': first_name, 'last_name': last_name})
+        else:
+            # If the film is not in the result list, add it along with its category and actor
+            film_data = {
+                'film_id': film_id,
+                'title': title,
+                'description': description,
+                'release_year': release_year,
+                'length': length,
+                'rating': rating,
+                'category_id': category_id,
+                'name': category_name,
+                'actors': [{'first_name': first_name, 'last_name': last_name}]
+            }
+            result.append(film_data)
+
+    return jsonify(result)
+
+
+
+
+@app.route('/listfilm/<film_id>', methods=['GET'])
+def listfilm(film_id):
+    # Query the Customer table to get details of the specified customer
+    films = Film.query.get(film_id)
+    if films:
+        language = Language.query.get(films.language_id)
+
+        film_data = {
+            'film_id': films.film_id,
+            'title': films.title,
+            'description': films.description,
+            'release_year': films.release_year,
+            'language_id': films.language_id,
+            'original_language_id': language.name,
+            'rental_duration': films.rental_duration,
+            'rental_rate': films.rental_rate,
+            'length': films.length,
+            'replacement_cost': films.replacement_cost,
+            'rating': films.rating,
+            'special_features': films.special_features,
+            'last_update': films.last_update
+
+        }
+        return jsonify(film_data)
+    else:
+        return jsonify({'message': 'Film not found'}), 404
 
 
 @app.route('/userupdate/<customer_id>', methods=['PUT'])
@@ -469,24 +643,29 @@ def useradd():
 
     # Query the City table to get the city ID based on the city name
     city = City.query.filter_by(city=city_name).first()
-    if not city: 
-        return jsonify({'message': 'City not found'}), 404
     
     new_city = City(city=city_name, country_id=1)
     db.session.add(new_city)
     db.session.commit()
+    new_city_id = new_city.city_id
+    print("This should be the new City: ",new_city_id)
 
     # Create a new Address object and add it to the database
-    new_address = Address(address=address, address2=address2, district=district, city_id=city.city_id, postal_code=postal_code, phone=phone)
+    new_address = Address(address=address, address2=address2, district=district, city_id=new_city_id, postal_code=postal_code, phone=phone)
     db.session.add(new_address)
     db.session.commit()
+    new_address_id = new_address.address_id
+    print("This should be the new address: ",new_address_id)
 
-    # Create a new Customer object and add it to the database
-    new_customer = Customer(first_name=first_name, last_name=last_name, email=email, address_id=new_address.id, store_id=1)
-    db.session.add(new_customer)
-    db.session.commit()
+    if new_address:
+        # Create a new Customer object and add it to the database
+        new_customer = Customer(first_name=first_name, last_name=last_name, email=email, address_id=new_address_id, store_id=1)
+        db.session.add(new_customer)
+        db.session.commit()
 
-    return jsonify({'message': 'User added successfully', 'user_id': new_customer.id}), 201
+        return jsonify({'message': 'User added successfully', 'user_id': new_customer.customer_id}), 201
+    else:
+        return jsonify({'message': 'Failed to add user'}), 500
 
 # Delete a user
 @app.route('/userdelete/<customer_id>', methods=['DELETE'])
@@ -508,11 +687,82 @@ def userdelete(customer_id):
         db.session.rollback()  # Rollback the transaction to avoid leaving the database in an inconsistent state
         return jsonify({'error': 'Could not delete user. Associated records exist.'}), 500
 
+@app.route('/searchusers', methods=['GET'])
+def search_users():
+    query = request.args.get('query', '')
+    # Query customers based on customer ID, first name, or last name
+    results = Customer.query.filter(
+        (Customer.customer_id.ilike(f'%{query}%')) |
+        (Customer.first_name.ilike(f'%{query}%')) |
+        (Customer.last_name.ilike(f'%{query}%'))
+    ).all()
+    # Serialize query results to JSON
+    serialized_results = [{
+        'customer_id': customer.customer_id,
+        'first_name': customer.first_name,
+        'last_name': customer.last_name,
+        'email': customer.email,
+        'last_update': customer.last_update
+    } for customer in results]
+    return jsonify(serialized_results)
+ 
+@app.route('/searchfilms', methods=['GET'])
+def search_films():
+    query = request.args.get('query', '')
 
+    # Filter films based on title, description, actor name, or category name
+    results = Film.query.join(FilmActor).join(Actor).join(FilmCategory).join(Category).filter(
+        (Film.title.ilike(f'%{query}%')) |
+        (Actor.first_name.ilike(f'%{query}%')) |
+        (Actor.last_name.ilike(f'%{query}%')) |
+        (Category.name.ilike(f'%{query}%'))
+    ).distinct().all()
 
+    # Serialize query results to JSON
+    serialized_results = [{
+        'film_id': film.film_id,
+        'title': film.title,
+        'description': film.description,
+        'release_year': film.release_year,
+        'length': film.length,
+        'rating': film.rating
+    } for film in results]
 
+    return jsonify(serialized_results)
+from flask import request
 
+@app.route('/userupdate/<customer_id>', methods=['POST'])
+def rentedupdate(customer_id):
+    # Get customer and movie details from the request
+    data = request.json
+    film_id = data.get('film_id')
 
+    # Return 400 if movie ID is not provided in the request
+    if not film_id:
+        return jsonify({'error': 'Movie ID not provided'}), 400
+
+    # Query the Customer table to get details of the specified customer
+    user = Customer.query.get(customer_id)
+    
+    # Return 404 if user is not found
+    if not user:
+        return jsonify({'error': 'Customer not found'}), 404
+
+    # Check if the movie is available for rent
+    rental = db.session.query(Rental).join(Inventory).filter(
+        Rental.customer_id == customer_id,
+        Inventory.film_id == film_id,
+        Rental.return_date == None
+    ).first()
+    
+    if rental:
+        return jsonify({'error': 'Movie is already rented by the customer'}), 400
+    else:
+        # Rent out the movie
+        rental = Rental(customer_id=customer_id, film_id=film_id, rental_date=datetime.now(), staff_id=1, return_date=None)
+        db.session.add(rental)
+        db.session.commit()
+        return jsonify({'message': 'Movie rented successfully'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
